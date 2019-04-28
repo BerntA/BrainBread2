a8795cfcd1d2039f3dfee251b8abb3f9fb417829
Bernt Andreas Eide <bernta1@msn.com>
2019-04-28 18:30:42 +0200

    - Applied OSX fixes.

--- game/shared/bb2/random_extended.cpp
+++ game/shared/bb2/random_extended.cpp
@@ -4,11 +4,15 @@
 //
 //========================================================================================//
 
-#include "random_extended.h"
+#ifdef OSX // Because OSX is AIDS, we won't let OSX have fun.
+#include "cbase.h"
+#else
 #include <random>
-
 static std::random_device rd;
 static std::mt19937 g(rd());
+#endif
+
+#include "random_extended.h"
 
 bool TryTheLuck(double percent)
 {
@@ -17,18 +21,31 @@ bool TryTheLuck(double percent)
 	else if (percent <= 0.0)
 		return false;
 
+#ifdef OSX
+	double tmp = round(percent * PERCENT_BASE);
+	return ((random->RandomInt(0, 100) <= ((int)tmp)));
+#else
 	std::bernoulli_distribution d(percent);
 	return d(g);
+#endif
 }
 
 int RandomIntegerNumber(int min, int max)
 {
+#ifdef OSX
+	return random->RandomInt(min, max);
+#else
 	std::uniform_int_distribution<> d(min, max);
 	return d(g);
+#endif
 }
 
 double RandomDoubleNumber(double min, double max)
 {
+#ifdef OSX
+	return (double)random->RandomFloat((float)min, (float)max);
+#else
 	std::uniform_real_distribution<> d(min, max);
 	return d(g);
+#endif
 }
