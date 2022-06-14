71da2744b48ca0acc6646986da53bb46fdacdce1
Code4Cookie <benjamin.m.bogacz@gmail.com>
2022-06-14 10:07:20 -0400

    Revert "Removed redundant debug messages"
    This reverts commit 4ddc38428425fe65e1796f87fc74c535760e6ec3.

--- game/server/ai_tacticalservices.cpp
+++ game/server/ai_tacticalservices.cpp
@@ -9,15 +9,12 @@
 #include "ai_moveprobe.h"
 #include "ai_pathfinder.h"
 #include "ai_navigator.h"
-#include "ai_waypoint.h"
 
 #ifdef BB2_USE_NAVMESH
 #include "nav.h"
 #include "nav_pathfind.h"
 #include "nav_area.h"
 #endif // BB2_USE_NAVMESH
-
-
 
 // memdbgon must be the last include file in a .cpp file!!!
 #include "tier0/memdbgon.h"
@@ -301,7 +298,6 @@ const Vector CAI_TacticalServices::FindLosNavArea(const Vector& vThreatPos, cons
 			if (GetOuter()->TestShootPosition(navOrigin, vThreatEyePos))
 				return navOrigin;
 		}
-		
 	}
 
 	return vec3_invalid;
